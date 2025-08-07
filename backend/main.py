4e2b4f2385dc2c1ed121962359313129af469be7
Ishaan <ishaan.s0709@gmail.com>
2025-08-07 13:12:19 +0530

    fix: updated uvicorn path

--- backend/main.py
+++ backend/main.py
@@ -35,4 +35,5 @@ def health():
     return {"message": "MedMentor API is live"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=10000, reload=True)
+
